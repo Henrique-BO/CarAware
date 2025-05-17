e5249fe8d1a11e6132c51c90b3261ca9f51fd839
Henrique Barros Oliveira <henrique.barrosoliveira@usp.br>
2025-05-17 17:58:26 -0300

    Fix entrypoints

--- entrypoint_ros_carla.sh
+++ entrypoint_ros_carla.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 python3 /root/carla_ws/src/CarAware/wait_for_carla.py
 
+# Wait to ensure CARLA-CarAware is fully up
+sleep 15
+
 source /opt/ros/$ROS_DISTRO/setup.bash
 source /root/carla_ws/install/setup.bash
 ros2 launch caraware_ros bringup_carla.launch.py $@
